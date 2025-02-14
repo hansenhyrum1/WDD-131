7eaa764f5bcbc0c9ed9ccbec0859f6cb06a233f2
hansenhyrum1 <han23044@byui.edu>
2025-02-14 11:38:46 -0700

    go

---
+++ Week 6/array.js
@@ -0,0 +1,39 @@
+
+
+const grades = ["A", "B", "A"];
+
+function convertGradeToPoints(grade) {
+    let points = 0;
+    if (grade === "A") {
+        points = 4;
+    }
+    else if (grade === "B") {
+        points = 3;
+    }
+    return points;
+}
+
+const gpaPoints = grades.map(convertGradeToPoints);
+console.log(gpaPoints);
+
+const pointsTotal = gpaPoints.reduce(function (total, item) {
+    return total + item;
+});
+const gpa = pointsTotal / gpaPoints.length;
+
+
+const words = ["watermelon", "peach", "apple", "tomato", "grape"];
+
+const shortWords = words.filter(function (word) {
+  return word.length < 6;
+});
+
+console.log(shortWords);
+
+
+const myArray = [12, 34, 21, 54];
+
+const luckyNumber = 21;
+let luckyIndex = myArray.indexOf(luckyNumber);
+
+console.log(luckyIndex);
